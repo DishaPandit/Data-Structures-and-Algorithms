14b6c073fd5fd1bfded9771670766c080aae1fea
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-10-15 23:04:55 +0530

    Updated

--- DSA/src/day5_IntroToJavaOperators/PrintMessgae.java
+++ DSA/src/day5_IntroToJavaOperators/PrintMessgae.java
@@ -1,6 +1,8 @@
 //https://www.scaler.com/academy/mentee-dashboard/class/34841/homework/problems/25453?navref=cl_tt_nv
 package day5_IntroToJavaOperators;
 
+import java.util.Scanner;
+
 public class PrintMessgae {
 
 	public static void main(String[] args) {
