391c620b95e6b721613a0ddd8273aa2cdc7e30dc
DISHA <71927399+DishaPandit@users.noreply.github.com>
2022-10-23 21:58:47 +0530

    Adding MCQ

---
+++ DSA/src/day2_InputOutput_Datatypes/MCQ.java
@@ -0,0 +1,124 @@
+package day2_InputOutput_Datatypes;
+
+public class MCQ {
+//	
+//		Ques 1
+//		
+//		What would be the output of the following snippet?
+//
+//				int a =10;
+//
+//				int b = 20;
+//
+//				int c = 30;
+//
+//				System.out.print(a);
+//
+//				System.out.println(b);
+//
+//				System.out.print(c);
+//
+//
+//				Note : \n represents output in newline
+//				
+//		Ans : Statement 1 would print 10.
+//		Statement 2 would print 20 in the same line since the cursor did not move to the next line.
+//
+//		Statement 3 would print 30 in the next line.
+//
+//		The final output would be:
+//		1020
+//		30
+//		
+//---------------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 2
+//		
+//		What will be the output of the following code ?
+//
+//				int g = 9;
+//				System.out.print(g * 3);
+//				
+//		Ans : The output will be the product of value of variable g with 3.
+//		Hence, the answer is 27.
+//		
+//--------------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 3
+//		
+//		Which of the following options is correct to declare int variables and use it ?
+//				
+//				1. int a ; System.out.print(a);
+//				2. int a = 10; System.out.print(a);
+//				3. Int a =20; System.out.print(a);
+//				4. None of the above are correct
+//				
+//		Ans : Option one is wrong : Because before using any variable in java, we need to initialize it first.
+//		Option three is wrong : I in int is capital, in java everything is case sensitive. 
+//		
+//---------------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 4
+//		
+//		Which of the following options will give you the following output?
+//
+//		Output: 1020
+//		
+//				1. System.out.println(10 + 20);
+//				2. system.out.println(1020);
+//				3. System.out.println(1020);
+//				4. None of the above
+//				
+//		Ans : Option 1 is wrong {This will give 30 as the output as it will add 10 and 20}
+//		Option 2 is wrong {Because the s in System should be capital as java is case sensitive}
+//		
+//-------------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 5
+//		
+//		Every statement in JAVA language should end with?
+//				
+//		Ans : Semi-colon [ ; ]
+//				
+//-----------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 6
+//		
+//		What is the correct output ?
+//
+//				int a = 5 + 5 * 2 + 2 * 2 + (2 * 3);
+//
+//				System.out.println(a);
+//				
+//		Ans : It is evaluated as 5 + 10 + 4 + 6
+//		
+//----------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 7
+//		
+//		What is the correct output ?
+//				int x = 10;
+//				System.out.print(x+1)
+//				System.out.print(x)
+//				
+//		Ans : 1110 is Correct because, just by writing x+1, x value won’t be updated. 
+//		If we want to increase x by 1, we need to assign a new value to x in this manner ? x = x+1
+//			
+//----------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 8
+//		
+//		Who invented JAVA language?
+//				
+//		Ans : James Gosling
+//		
+//---------------------------------------------------------------------------------------------------------------------------------
+//
+//		Ques 9
+//		
+//		JAVA is a successor to which programming language?
+//				
+//		Ans : C++ is Correct option → {C++ introduced OOPs (Object Oriented Programming) concepts which are adopted by Java.}
+
+
+}
